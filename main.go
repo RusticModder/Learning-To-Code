72706fd99664f1b31b6e867c55ab50ccdf427f21
caleb <caleb@coding-ninja.com>
2023-01-02 18:03:42 -0500

    initial

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// This is a comment
+
+/* Caleb's Adventure (To be determined)
+
+Things our game needs to have:
+1. Player
+	a) Fight
+	b) Heal
+	c) Collect Items
+	d) interact with envrionment
+	e) armor system
+2. NPC
+	a) bad guys
+	b) Merchants
+3. Envrionment
+	a) Trees
+	b) Houses
+*/
+
+func main() {
+	// Start
+	// Variable
+	gameName := "Caleb's Adventure"
+	//fmt.Printf("Game Name: %s", gameName)
+	fmt.Printf("Game Name: %s", gameName)
+}
